7c99273a91f6fb424d831243e5e01b8dcf5d47cc
Krens000 <klimbasiya667@rku.ac.in>
2025-08-26 18:32:26 +0530

    chore(#8): edited helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,6 @@
-def format_label(class_id):
-    label_map = {0: "Human", 1: "Vehicle"}
-    return label_map.get(class_id, "Unknown")
+import os
+
+def ensure_dir(path):
+    """Create directory if not exists"""
+    if not os.path.exists(path):
+        os.makedirs(path)
